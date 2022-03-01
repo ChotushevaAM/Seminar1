be514610ec55d5f78b51a631636af0a794b544fa
Anara <anardi.chotusheva@mail.ru>
2022-03-01 21:53:18 +0300

    1 exercise added

---
+++ Seminar1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int kvadrat_value = value * value;
+Console.WriteLine("Квадрат данного числа равен");
+Console.WriteLine(kvadrat_value);
+
